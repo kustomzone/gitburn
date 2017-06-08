764c21a8306c2b1f638ee5206c20985df5d6bdd1
Unknown <ukrgametactics@ex.ua>
2017-06-08 14:03:02 +0300

    Fix

--- app/webpack.common.js
+++ app/webpack.common.js
@@ -2,7 +2,7 @@
 
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CleanWebpackPlguin = require('clean-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 
