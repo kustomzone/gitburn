51d2029f07010196887d729b9e04b2db0db4408f
Vlad Hashimoto <ukrgametactics@ex.ua>
2017-06-11 11:08:21 +0300

    Add Crash Window

--- app/src/main-process/main.ts
+++ app/src/main-process/main.ts
@@ -117,7 +117,7 @@ if (shellNeedsPatching(process)) {
 const isDuplicateInstance = app.makeSingleInstance((commandLine, workingDirectory) => {
   // Someone tried to run a second instance, we should focus our window.
   if (mainWindow) {
-    if (mainWindow.inMinimized()) {
+    if (mainWindow.isMinimized()) {
       mainWindow.restore()
     }
     mainWindow.focus()
